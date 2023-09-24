4bea2ee4a66869458f5e487449f552cf886f7de0
zschuah <zschuah@hotmail.com>
2023-09-24 21:36:07 +0800

    completed layout for all tabs

--- src/pages/profile/PersonalPreferencesEdit.tsx
+++ src/pages/profile/PersonalPreferencesEdit.tsx
@@ -1,8 +1,31 @@
+import { useProfileContext } from "../../context/ProfileContext";
+import InputGray from "../../layout/InputGray";
+
 const PersonalPreferncesEdit = () => {
+  const { profile } = useProfileContext();
+
   return (
-    <div>
-      <h3>PersonalPreferncesEdit</h3>
-    </div>
+    <section className="py-8 space-y-4">
+      <div>
+        <p className="font-bold">Hobbies and interests</p>
+        <InputGray defaultValue={profile.hobbies} />
+      </div>
+
+      <div>
+        <p className="font-bold">Favorite sport(s)</p>
+        <InputGray defaultValue={profile.sports} />
+      </div>
+
+      <div>
+        <p className="font-bold">Preferred music genre(s)</p>
+        <InputGray defaultValue={profile.music} />
+      </div>
+
+      <div>
+        <p className="font-bold">Preferred movie/TV show(s)</p>
+        <InputGray defaultValue={profile.movies} />
+      </div>
+    </section>
   );
 };
 
--- src/pages/profile/SpouseDetailsEdit.tsx
+++ src/pages/profile/SpouseDetailsEdit.tsx
@@ -1,8 +1,34 @@
+import { useProfileContext } from "../../context/ProfileContext";
+import InputGray from "../../layout/InputGray";
+import SelectGray from "../../layout/SelectGray";
+
 const SpouseDetailsEdit = () => {
+  const { profile } = useProfileContext();
+
   return (
-    <div>
-      <h3>SpouseDetailsEdit</h3>
-    </div>
+    <section className="py-8 space-y-4 inline-block">
+      <div>
+        <p className="font-bold">Salutation</p>
+        <SelectGray defaultValue={profile.spouseSalutation} className="w-full">
+          <option value="" disabled hidden>
+            Select salutation
+          </option>
+          <option value="Mr.">Mr.</option>
+          <option value="Ms.">Ms.</option>
+          <option value="Mrs.">Mrs.</option>
+        </SelectGray>
+      </div>
+
+      <div>
+        <p className="font-bold">First name</p>
+        <InputGray defaultValue={profile.spouseFirstName} />
+      </div>
+
+      <div>
+        <p className="font-bold">Last name</p>
+        <InputGray defaultValue={profile.spouseLastName} />
+      </div>
+    </section>
   );
 };
 
